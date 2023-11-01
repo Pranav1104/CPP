c5ca2e57ae82a677b09985c921dfb38fcb82eb9b
Pranav1104 <95237907+Pranav1104@users.noreply.github.com>
2023-11-01 22:31:04 +0530

    basic cpp

---
+++ cwh/basic.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+
+int main()
+{
+   int num1, num2;
+   cout<<"enter the number num1:\n";
+   cin>>num1;
+   cout<<"enter the number num2:\n";
+   cin>>num2;
+   cout<<"the sum of 2 numbers is "<<num1+num2;
+
+   return 0;
+}
