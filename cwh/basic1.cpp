b3dae1f9da3134990a225a3afaf85e8486a3b9b9
Pranav1104 <95237907+Pranav1104@users.noreply.github.com>
2023-11-01 22:31:28 +0530

    basic cpp

---
+++ cwh/basic1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int glo = 6;
+void sum(){
+    int a;
+    }
+
+int main(){
+    int a=5, b=6;
+    float c=2.24;
+    int glo= 12;
+    glo=50; 
+    // cout<<"the value of a is "<<a<<"\nthe value of b is "<<b;
+    // cout<<"\nthe valu of c is "<<c;
+    cout<< glo;
+    return 0;
+}
