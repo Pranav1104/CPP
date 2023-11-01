441e554892ae22efd94c29908bf65dbfabc4a913
Pranav1104 <95237907+Pranav1104@users.noreply.github.com>
2023-11-01 09:56:04 +0530

    Create arrays.cpp

---
+++ cwh/arrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[4]={23, 24, 25, 26};
+    // marks[2]= 258;
+    // cout<<marks[2]<<endl;
+    // for (int i=0; i<4; i++){
+    //     cout<<"the value of "<<i<<" is "<<marks[i]<<endl;
+        
+    // }
+
+    // while loop
+    // int i=0;
+    // while (i<4)
+    // {
+    //     cout<<"the value of "<<i<<" is "<<marks[i]<<endl;
+    //     i++;
+    // }
+    // do while loop
+    // int i=0;
+    // do
+    // {
+    //      cout<<"the value of "<<i<<" is "<<marks[i]<<endl;
+    //      i++;
+    // } while (i<4);
+    
+    
+    return 0;
+}
