711b17d6a6a55b3021e5a5f5dda64eb12d1984ce
Pranav1104 <95237907+Pranav1104@users.noreply.github.com>
2023-11-01 22:31:47 +0530

    Create cwh10.cpp

---
+++ cwh/cwh10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// for loop example
+int main(){
+ for(int i=1,a=10; i<=10; i++)
+    {
+        cout<<i*a<<endl;
+    }
+//  return 0;
+// }
+// while loop 
+// int i=1,a=7;
+// while (i<=10)
+//     {
+//         cout<<i*a<<endl;
+//         i++;
+
+//     }
+//  return 0;
+// }
+// int i=1,a=6;
+// do{
+//     cout<<i*a<<endl;
+//     i++;
+// }while(i<=10);
+
+    return 0;
+}
